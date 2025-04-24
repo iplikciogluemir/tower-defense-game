9aea07ecfde5e646da606429a8f0fc31a36241f0
Emir İplikcioğlu <iplikciogluemir@gmail.com>
2025-04-24 18:36:04 +0300

    improved tower side panel

--- src/main/java/com/towerdefense/ui/TowerPanel.java
+++ src/main/java/com/towerdefense/ui/TowerPanel.java
@@ -3,6 +3,8 @@ package com.towerdefense.ui;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 
+import com.towerdefense.towers.SingleShotTower;
+
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
@@ -26,10 +28,10 @@ public class TowerPanel extends Application {
     public VBox getTowerPanel() {
         VBox vbx = new VBox(10);
 
-        Group tower = new Group(new Rectangle(50, 50, Color.BLACK));
+        // Group tower = new Group(new Rectangle(50, 50, Color.BLACK));
 
         vbx.setStyle("-fx-background-color: #faf1da");
-        Label singleShotTower = new Label("Single Shot Tower - 50$", tower);
+        Label singleShotTower = new Label("Single Shot Tower - 50$", SingleShotTower.getSingleShotTower());
         singleShotTower.setContentDisplay(ContentDisplay.TOP);
         singleShotTower.setStyle("-fx-background-color: #f2d79d;" +
                 "-fx-border-color: #eed399;" +
