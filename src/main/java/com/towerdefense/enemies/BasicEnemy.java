892ddefbe3ff94ed1a3be1b3971ad5e6207d81bb
Emir İplikcioğlu <iplikciogluemir@gmail.com>
2025-04-24 15:29:24 +0300

    added package names, and some minor adjustments

---
+++ src/main/java/com/towerdefense/enemies/BasicEnemy.java
@@ -0,0 +1,1 @@
+package com.towerdefense.enemies;
--- src/main/java/com/towerdefense/ui/GameUI.java
+++ src/main/java/com/towerdefense/ui/GameUI.java
@@ -1,4 +1,4 @@
-package main.java.com.towerdefense.ui;
+package com.towerdefense.ui;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
