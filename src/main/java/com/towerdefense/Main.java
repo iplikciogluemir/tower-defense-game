7667f86531ec5cb5357f09b4867b248bd338a8d3
AlperKerimELMAS <alperelmas0@gmail.com>
2025-04-26 15:24:28 +0300

    Minor adjustments on MapCell class

--- src/main/java/com/towerdefense/Main.java
+++ src/main/java/com/towerdefense/Main.java
@@ -1,1 +1,26 @@
 package com.towerdefense;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import com.towerdefense.map.MapCell;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws FileNotFoundException, IOException {
+
+        BorderPane mainPane = MapCell.getMap(1);
+        Scene scene = new Scene(mainPane, 500, 500);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
