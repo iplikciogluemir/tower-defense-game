969f3e22d13e0774d7f304a7c562a565237466d3
Nihat Samet Özyiğit <sametozygit2006@gmail.com>
2025-04-28 21:21:52 +0300

    auto fullscreen

--- src/main/java/com/towerdefense/Main.java
+++ src/main/java/com/towerdefense/Main.java
@@ -13,12 +13,14 @@ import javafx.application.Application;
 import javafx.geometry.Point2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.util.Duration;
 
 public class Main extends Application {
@@ -37,12 +39,15 @@ public class Main extends Application {
         StackPane mainPane = new StackPane(uiPane, mainDraggablePane);
 
         Scene scene = new Scene(mainPane, 500, 500);
+        
+        //primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setScene(scene);
+        primaryStage.setFullScreen(true);
         primaryStage.show();
 
         Group enemyTest = Enemy.getEnemy();
         uiPane.getChildren().addAll(enemyTest);
-
+        
         uiPane.setOnMouseClicked(e -> {
             PathTransition pathTransition = new PathTransition();
             pathTransition.setDuration(Duration.seconds(2));
@@ -56,6 +61,8 @@ public class Main extends Application {
             pathTransition.setCycleCount(PathTransition.INDEFINITE);
             
             pathTransition.play();
+
+
 
             // Point2D firstTile = (((GridPane)uiPane.getCenter()).getChildren().get(0)).localToScene(0,0);
             // Circle circle = new Circle(firstTile.getX() , firstTile.getY(), 60);
