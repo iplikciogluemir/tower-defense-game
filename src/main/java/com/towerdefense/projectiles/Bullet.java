443471849b7c08f9d0bb363a2ffeb5769de773c0
Emir İplikcioğlu <iplikciogluemir@gmail.com>
2025-05-02 14:08:50 +0300

    updated bullet shooting

--- src/main/java/com/towerdefense/projectiles/Bullet.java
+++ src/main/java/com/towerdefense/projectiles/Bullet.java
@@ -1,50 +1,66 @@
 package com.towerdefense.projectiles;
 
-import javafx.animation.TranslateTransition;
+import com.towerdefense.enemies.Enemy;
+
+import javafx.animation.PathTransition;
+import javafx.animation.Timeline;
+import javafx.animation.KeyFrame;
 import javafx.application.Application;
 import javafx.beans.property.DoubleProperty;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-import javafx.scene.paint.Color;
 
 public class Bullet extends Application {
+
     @Override
     public void start(Stage primaryStage) {
-        Pane pane = new Pane();
-        shoot(pane, 50, 50, 100, 100);
-        Scene scene = new Scene(pane, 200, 200);
-        primaryStage.setTitle("test");
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }
 
-    public void shoot(Pane pane, double towerX, double towerY, double enemyX, double enemyY) {
+    public void shootBullet(Pane pane, double towerX, double towerY, Group enemy) {
         Circle bullet = new Circle(5, Color.RED);
         bullet.setCenterX(towerX);
         bullet.setCenterY(towerY);
-
-        double dX = enemyX - towerX;
-        double dY = enemyY - towerY;
-        double distance = Projectile.getDistance(towerX, towerY, enemyX, enemyY);
-
-        TranslateTransition bulletTransition = new TranslateTransition();
-        bulletTransition.setNode(bullet);
-        bulletTransition.setDuration(Duration.seconds(distance / 200));
-        bulletTransition.setByX(dX);
-        bulletTransition.setByY(dY);
-        bulletTransition.setOnFinished(e -> {
-            ((Pane) bullet.getParent()).getChildren().remove(bullet);
-        });
-
         pane.getChildren().add(bullet);
 
-        bulletTransition.play();
-    }
+        final double speed = 100.0;
+        final double interval = 20;
 
-    public static void main(String[] args) {
-        launch(args);
+        // needed to enable modification
+        final Timeline[] timelineRef = new Timeline[1];
+
+        timelineRef[0] = new Timeline(new KeyFrame(Duration.millis(interval), e -> {
+            double targetX = enemy.getTranslateX();
+            double targetY = enemy.getTranslateY();
+            double currentX = bullet.getCenterX();
+            double currentY = bullet.getCenterY();
+
+            double moveX = targetX - currentX;
+            double moveY = targetY - currentY;
+
+            double distance = Math.hypot(moveX, moveY);
+
+            // Check collision
+            if (distance < 10) {
+                pane.getChildren().remove(bullet);
+                // decrease enemy health
+                timelineRef[0].stop();
+                return;
+            }
+
+            double stepSize = speed / interval;
+            double moveStepX = (moveX / distance) * stepSize;
+            double moveStepY = (moveY / distance) * stepSize;
+
+            bullet.setCenterX(currentX + moveStepX);
+            bullet.setCenterY(currentY + moveStepY);
+        }));
+        timelineRef[0].setCycleCount(Timeline.INDEFINITE);
+        timelineRef[0].play();
     }
 }
