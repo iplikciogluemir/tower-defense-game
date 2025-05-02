ed0cbb5986ccf4b5d4067fef225a6ec9a7e600c3
Nihat Samet Özyiğit <sametozygit2006@gmail.com>
2025-05-02 21:10:02 +0300

    i am killing myself

---
+++ src/main/java/com/towerdefense/ui/GameColors.java
@@ -0,0 +1,23 @@
+package com.towerdefense.ui;
+
+
+public class GameColors {
+    public static String BackGround = "";
+    public static String TilesColor = "";
+    public static String TowerColor1 = "";
+    public static String TowerColor2 = "";
+
+    public static void SetBackGround(String newColor){
+        GameColors.BackGround = newColor;
+    }
+    public static void SetTilesColor(String newColor){
+        GameColors.TilesColor = newColor;
+    }
+    public static void SetTowerColor1(String newColor){
+        GameColors.TowerColor1 = newColor;
+    }
+    public static void SetTowerColor2(String newColor){
+        GameColors.TowerColor2 = newColor;
+    }
+    
+}
